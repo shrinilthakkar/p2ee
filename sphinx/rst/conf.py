8b3d5a56c857f1de6d53c1a36c457a345ce7a7e6
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-21 15:26:27 +0530

    Update conf.py

--- sphinx/rst/conf.py
+++ sphinx/rst/conf.py
@@ -17,6 +17,10 @@ import sys
 from os.path import dirname as up
 code_path = os.path.join(up(up(up(__file__))), '')
 sys.path.append(code_path)
+print code_path + "%%%%%%"
+code_path = os.path.join(up(up(up(__file__))), '/p2ee')
+sys.path.append(code_path)
+print code_path + "$$$$$"
 
 
 # -- Project information -----------------------------------------------------
