17adbf58d12a65113af44c879c8c1ddc257e2a02
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-21 12:23:21 +0530

    Update conf.py

--- sphinx/rst/conf.py
+++ sphinx/rst/conf.py
@@ -44,6 +44,9 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
