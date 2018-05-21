0a2911ba60078a1ab3aa03932bdf591a3989fc62
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-21 14:29:03 +0530

    Create hello.py

---
+++ p2ee/hello.py
@@ -0,0 +1,17 @@
+def func_1():
+    """
+    This is the func 1 inside scripts folder.
+    """
+    print "print statement for hello"
+
+def func_2():
+    """
+    This is the func 2 inside scripts folder.
+    """
+    print "print statement for func2"
+
+def func_3():
+    """
+    This is the func 3 inside scripts folder.
+    """
+    print "func3 in hello.py"
