db88693d00515c1b3790dcc3363f33070fb3a7f7
shrinilthakkar <35483071+shrinilthakkar@users.noreply.github.com>
2018-05-22 14:58:12 +0530

    Update conf.py

--- sphinx/rst/conf.py
+++ sphinx/rst/conf.py
@@ -15,8 +15,10 @@
 import os
 import sys
 from os.path import dirname as up
-code_path = os.path.join(up(up(up(__file__))), '')
+code_path = up(up(up(__file__)))
 sys.path.append(code_path)
+print "&&&&&&&&&&&&&"
+print sys.path
 
 # -- Project information -----------------------------------------------------
 
