ec2365edff28e7d4edabf63ce15d16328812a570
shrinilthakkar <shrinil@moengage.com>
2018-05-30 18:15:21 +0530

    added treysor

---
+++ p2ee/models/exceptions.py
@@ -0,0 +1,44 @@
+class InvalidFieldException(Exception):
+    def __init__(self, message='Invalid field', field=None, missing=True):
+        self.field = field
+        self.missing = missing
+
+        if self.field is not None:
+            message = "{field}: {message}".format(field=field, message=message)
+
+        super(InvalidFieldException, self).__init__(message)
+
+    def get_message(self):
+        message = ''
+
+        if self.field is not None:
+            message = "{field}: ".format(field=self.field)
+
+        if self.missing:
+            return "{field}{message}".format(field=message,
+                                             message='Missing field(s)').strip(':').strip(' ')
+        return "{field}{message}".format(
+            field=message,
+            message='Field(s) is/are not allowed'
+        ).strip(':').strip(' ')
+
+
+class InvalidFieldValueException(Exception):
+    def __init__(self, message='Invalid field value', field=None, value=None):
+        if field is not None:
+            message = "{field}: {message}".format(field=field,
+                                                  message=message).strip(':').strip(' ')
+
+        super(InvalidFieldValueException, self).__init__(message)
+
+    def get_message(self):
+        return self.message
+
+
+class InvalidFieldDefinition(Exception):
+    def __init__(self, message='Invalid Field Definition', field=None):
+        self.field = field
+        super(InvalidFieldDefinition, self).__init__(message + ': ' + str(field))
+
+    def get_message(self):
+        return self.message
---
+++ p2ee/threadsafe/__init__.py
@@ -0,0 +1,62 @@
+import threading
+import os
+import socket
+from weakref import WeakKeyDictionary
+from p2ee.utils.common_utils import CommonUtils
+from p2ee.utils.package_utils import PackageUtils
+
+
+class ThreadContext(object):
+    def __init__(self, **kwargs):
+        """
+        Maintains thread level contexts
+        :param kwargs: contains the default global context
+        """
+        self._thread_contexts = WeakKeyDictionary()
+        self._global_context = dict(**kwargs)
+
+    def __get_thread_key(self):
+        try:
+            from greenlet import getcurrent
+            return getcurrent()
+        except ImportError:
+            return threading.current_thread()
+
+    def __get_thread_context(self):
+        """
+        :return: context of the current thread, if no context exists, create one with global context values
+        """
+        thread_id = self.__get_thread_key()
+        return self._thread_contexts.setdefault(thread_id, dict(self._global_context))
+
+    def get(self, key):
+        return self.__get_thread_context().get(key)
+
+    def set(self, key, value):
+        self.__get_thread_context()[key] = value
+
+    def unset(self, key):
+        self.__get_thread_context().pop(key, None)
+
+    def updateContext(self, **kwargs):
+        self.__get_thread_context().update(**kwargs)
+
+    def removeContext(self, *keys):
+        for key in keys:
+            self.unset(key)
+
+    def clearContext(self):
+        thread_id = self.__get_thread_key()
+        self._thread_contexts[thread_id] = dict(self._global_context)
+
+    def to_dict(self):
+        return self.__get_thread_context()
+
+
+GLOBAL_CONTEXT = {
+    'correlationId': CommonUtils.generateRandomString(6),
+    'pid': str(os.getpid()),
+    'host': socket.gethostbyname(socket.gethostname()),
+    'env': PackageUtils.getExecutionEnv(),
+    'region': PackageUtils.getInstanceMeta()['region']
+}
